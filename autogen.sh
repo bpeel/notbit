2b4886baa3aac621b1c7aabf00106e0c8361afac
Neil Roberts <bpeeluk@yahoo.co.uk>
2013-12-03 11:36:57 +0000

    Add a proper autogen.sh that doesn't depend on gnome

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,36 @@
 #!/bin/sh
 
-REQUIRED_AUTOMAKE_VERSION=1.10 exec gnome-autogen.sh $@
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
+
+olddir=`pwd`
+
+cd "$srcdir"
+
+test -f src/main.c || {
+	echo "You must run this script in the top-level source directory"
+	exit 1
+}
+
+AUTORECONF=`which autoreconf`
+if test -z $AUTORECONF; then
+        echo "*** No autoreconf found, please install it ***"
+        exit 1
+fi
+
+# NOCONFIGURE is used by gnome-common
+if test -z "$NOCONFIGURE"; then
+        if test -z "$*"; then
+                echo "I am going to run ./configure with no arguments -" \
+                    "if you wish "
+                echo "to pass any to it, please specify them on the $0" \
+                    "command line."
+        fi
+fi
+
+rm -rf autom4te.cache
+
+autoreconf --force --install --verbose || exit $?
+
+cd "$olddir"
+test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
