26e3d9ba36887276a4fc42e64fc0c8de587c900f
Neil Roberts <bpeeluk@yahoo.co.uk>
2013-12-03 19:32:37 +0000

    Add a ref-counted type to represent a blob of data

---
+++ src/ntb-blob.c
@@ -0,0 +1,79 @@
+/*
+ * Notbit - A Bitmessage client
+ * Copyright (C) 2013  Neil Roberts
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "config.h"
+
+#include <string.h>
+
+#include "ntb-blob.h"
+#include "ntb-util.h"
+
+struct ntb_blob *
+ntb_blob_new(const void *data,
+             size_t size)
+{
+        struct ntb_blob *blob =
+                ntb_alloc(NTB_STRUCT_OFFSET(struct ntb_blob, data) + size);
+
+        blob->ref_count = 1;
+        blob->size = size;
+
+#ifndef HAVE_SYNC_REF_COUNT
+        pthread_mutex_init(&blob->ref_count_mutex, NULL);
+#endif
+
+        memcpy(blob->data, data, size);
+
+        return blob;
+}
+
+struct ntb_blob *
+ntb_blob_ref(struct ntb_blob *blob)
+{
+#ifdef HAVE_SYNC_REF_COUNT
+        __sync_fetch_and_add(&blob->ref_count, 1);
+#else
+        pthread_mutex_lock(&blob->ref_count_mutex);
+        blob->ref_count++;
+        pthread_mutex_unlock(&blob->ref_count_mutex);
+#endif
+
+        return blob;
+}
+
+void
+ntb_blob_unref(struct ntb_blob *blob)
+{
+        int old_value;
+
+#ifdef HAVE_SYNC_REF_COUNT
+        old_value = __sync_fetch_and_sub(&blob->ref_count, 1);
+#else
+        pthread_mutex_lock(&blob->ref_count_mutex);
+        old_value = blob->ref_count--;
+        pthread_mutex_unlock(&blob->ref_count_mutex);
+#endif
+
+        if (old_value <= 1) {
+#ifndef HAVE_SYNC_REF_COUNT
+                pthread_mutex_destroy(&blob->ref_count_mutex);
+#endif
+
+                ntb_free(blob);
+        }
+}
---
+++ src/ntb-blob.h
@@ -0,0 +1,58 @@
+/*
+ * Notbit - A Bitmessage client
+ * Copyright (C) 2013  Neil Roberts
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef NTB_BLOB_H
+#define NTB_BLOB_H
+
+#ifndef HAVE_SYNC_REF_COUNT
+#include <pthread.h>
+#endif /* HAVE_SYNC_REF_COUNT */
+
+#include <stdint.h>
+#include <stddef.h>
+
+/* A blob represents a ref-counted immutable chunk of data. This will
+ * be used to hold all inventory objects from the network such as
+ * messages and public keys. The ref-count is thread-safe so that the
+ * blob can be passed off to the store thread to be written to
+ * disk. */
+
+struct ntb_blob {
+#ifndef HAVE_SYNC_REF_COUNT
+        pthread_mutex_t ref_count_mutex;
+#endif /* HAVE_SYNC_REF_COUNT */
+
+        int ref_count;
+
+        size_t size;
+
+        /* Over-allocated to contain the data */
+        uint8_t data[1];
+};
+
+struct ntb_blob *
+ntb_blob_new(const void *data,
+             size_t size);
+
+struct ntb_blob *
+ntb_blob_ref(struct ntb_blob *blob);
+
+void
+ntb_blob_unref(struct ntb_blob *blob);
+
+#endif /* NTB_BLOB_H */
