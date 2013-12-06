d76cd58c79ee8cfc9b616d66d80616631a7bb644
Neil Roberts <bpeeluk@yahoo.co.uk>
2013-12-06 10:08:19 +0000

    Make ntb_main_context safe against adding idle in another thread

--- src/ntb-main-context.c
+++ src/ntb-main-context.c
@@ -29,6 +29,7 @@
 #include <stdbool.h>
 #include <limits.h>
 #include <time.h>
+#include <pthread.h>
 
 #include "ntb-main-context.h"
 #include "ntb-list.h"
@@ -47,6 +48,16 @@ ntb_main_context_error;
 struct ntb_main_context_bucket;
 
 struct ntb_main_context {
+        /* This mutex only guards access to n_sources, the
+         * idle_sources list and the slice allocator so that idle
+         * sources can be added from other threads. Everything else
+         * should only be accessed from the main thread so it doesn't
+         * need to guarded. Removing an idle source can only happen in
+         * the main thread. That is necessary because it is difficult
+         * to cope with random idle sources being removed while we are
+         * iterating the list */
+        pthread_mutex_t idle_mutex;
+
         int epoll_fd;
         /* Number of sources that are currently attached. This is used so we
            can size the array passed to epoll_wait to ensure it's possible
@@ -62,8 +73,10 @@ struct ntb_main_context {
 
         struct ntb_list idle_sources;
 
-        struct ntb_main_context_source *quit_pipe_source;
-        int quit_pipe[2];
+        struct ntb_main_context_source *async_pipe_source;
+        int async_pipe[2];
+        pthread_t main_thread;
+
         void (* old_int_handler)(int);
         void (* old_term_handler)(int);
 
@@ -75,6 +88,9 @@ struct ntb_main_context {
 
         struct ntb_list buckets;
         int64_t last_timer_time;
+
+        /* This allocator is protected by the idle_mutex */
+        struct ntb_slice_allocator source_allocator;
 };
 
 struct ntb_main_context_source {
@@ -122,8 +138,6 @@ struct ntb_main_context_bucket {
         int minutes_passed;
 };
 
-NTB_SLICE_ALLOCATOR(struct ntb_main_context_source,
-                    ntb_main_context_source_allocator);
 NTB_SLICE_ALLOCATOR(struct ntb_main_context_bucket,
                     ntb_main_context_bucket_allocator);
 
@@ -153,6 +167,80 @@ ntb_main_context_get_default_or_abort(void)
         return mc;
 }
 
+static void
+async_pipe_cb(struct ntb_main_context_source *source,
+              int fd,
+              enum ntb_main_context_poll_flags flags,
+              void *user_data)
+{
+        struct ntb_main_context *mc = user_data;
+        struct ntb_main_context_source *quit_source;
+        ntb_main_context_quit_callback callback;
+        uint8_t byte;
+
+        if (read(mc->async_pipe[0], &byte, sizeof(byte)) == -1) {
+                if (errno != EAGAIN && errno != EWOULDBLOCK && errno != EINTR)
+                        ntb_warning("Read from quit pipe failed: %s",
+                                    strerror(errno));
+        } else if (byte == 'Q') {
+                ntb_list_for_each(quit_source, &mc->quit_sources, quit_link) {
+                        callback = quit_source->callback;
+                        callback(quit_source, quit_source->user_data);
+                }
+        }
+}
+
+static void
+send_async_byte(struct ntb_main_context *mc,
+                char byte)
+{
+        while (write(mc->async_pipe[1], &byte, 1) == -1 && errno == EINTR);
+}
+
+static void
+ntb_main_context_quit_signal_cb(int signum)
+{
+        struct ntb_main_context *mc = ntb_main_context_get_default_or_abort();
+
+        send_async_byte(mc, 'Q');
+}
+
+static void
+init_main_context(struct ntb_main_context *mc,
+                  int fd)
+{
+        pthread_mutex_init(&mc->idle_mutex, NULL /* attrs */);
+        ntb_slice_allocator_init(&mc->source_allocator,
+                                 sizeof(struct ntb_main_context_source),
+                                 NTB_ALIGNOF(struct ntb_main_context_source));
+        mc->epoll_fd = fd;
+        mc->n_sources = 0;
+        mc->events = NULL;
+        mc->events_size = 0;
+        mc->monotonic_time_valid = false;
+        mc->wall_time_valid = false;
+        ntb_list_init(&mc->quit_sources);
+        ntb_list_init(&mc->idle_sources);
+        ntb_list_init(&mc->buckets);
+        mc->last_timer_time = ntb_main_context_get_monotonic_clock(mc);
+
+        mc->old_int_handler = signal(SIGINT, ntb_main_context_quit_signal_cb);
+        mc->old_term_handler = signal(SIGTERM, ntb_main_context_quit_signal_cb);
+
+        if (pipe(mc->async_pipe) == -1) {
+                ntb_warning("Failed to create pipe: %s",
+                            strerror(errno));
+        } else {
+                mc->async_pipe_source
+                        = ntb_main_context_add_poll(mc, mc->async_pipe[0],
+                                                    NTB_MAIN_CONTEXT_POLL_IN,
+                                                    async_pipe_cb,
+                                                    mc);
+        }
+
+        mc->main_thread = pthread_self();
+}
+
 struct ntb_main_context *
 ntb_main_context_new(struct ntb_error **error)
 {
@@ -178,17 +266,7 @@ ntb_main_context_new(struct ntb_error **error)
         } else {
                 struct ntb_main_context *mc = ntb_alloc(sizeof *mc);
 
-                mc->epoll_fd = fd;
-                mc->n_sources = 0;
-                mc->events = NULL;
-                mc->events_size = 0;
-                mc->monotonic_time_valid = false;
-                mc->wall_time_valid = false;
-                ntb_list_init(&mc->quit_sources);
-                ntb_list_init(&mc->idle_sources);
-                mc->quit_pipe_source = NULL;
-                ntb_list_init(&mc->buckets);
-                mc->last_timer_time = ntb_main_context_get_monotonic_clock(mc);
+                init_main_context(mc, fd);
 
                 return mc;
         }
@@ -214,12 +292,16 @@ ntb_main_context_add_poll(struct ntb_main_context *mc,
                           ntb_main_context_poll_callback callback,
                           void *user_data)
 {
-        struct ntb_main_context_source *source =
-                ntb_slice_alloc(&ntb_main_context_source_allocator);
+        struct ntb_main_context_source *source;
         struct epoll_event event;
 
         if (mc == NULL)
                 mc = ntb_main_context_get_default_or_abort();
+
+        pthread_mutex_lock(&mc->idle_mutex);
+        source = ntb_slice_alloc(&mc->source_allocator);
+        mc->n_sources++;
+        pthread_mutex_unlock(&mc->idle_mutex);
 
         source->mc = mc;
         source->fd = fd;
@@ -234,8 +316,6 @@ ntb_main_context_add_poll(struct ntb_main_context *mc,
                 ntb_warning("EPOLL_CTL_ADD failed: %s", strerror(errno));
 
         source->current_flags = flags;
-
-        mc->n_sources++;
 
         return source;
 }
@@ -263,68 +343,20 @@ ntb_main_context_modify_poll(struct ntb_main_context_source *source,
         source->current_flags = flags;
 }
 
-static void
-quit_pipe_cb(struct ntb_main_context_source *source,
-             int fd,
-             enum ntb_main_context_poll_flags flags,
-             void *user_data)
-{
-        struct ntb_main_context *mc = user_data;
-        struct ntb_main_context_source *quit_source;
-        ntb_main_context_quit_callback callback;
-        uint8_t byte;
-
-        if (read(mc->quit_pipe[0], &byte, sizeof(byte)) == -1) {
-                if (errno != EAGAIN && errno != EWOULDBLOCK && errno != EINTR)
-                        ntb_warning("Read from quit pipe failed: %s",
-                                    strerror(errno));
-        } else {
-                ntb_list_for_each(quit_source, &mc->quit_sources, quit_link) {
-                        callback = quit_source->callback;
-                        callback(quit_source, quit_source->user_data);
-                }
-        }
-}
-
-static void
-ntb_main_context_quit_signal_cb(int signum)
-{
-        struct ntb_main_context *mc = ntb_main_context_get_default_or_abort();
-        uint8_t byte = 42;
-
-        while (write(mc->quit_pipe[1], &byte, 1) == -1 && errno == EINTR);
-}
-
-static void
-create_quit_pipe_source(struct ntb_main_context *mc)
-{
-        if (pipe(mc->quit_pipe) == -1)
-                ntb_warning("Failed to create quit pipe: %s",
-                            strerror(errno));
-        else {
-                mc->quit_pipe_source
-                        = ntb_main_context_add_poll(mc, mc->quit_pipe[0],
-                                                    NTB_MAIN_CONTEXT_POLL_IN,
-                                                    quit_pipe_cb,
-                                                    mc);
-
-                mc->old_int_handler =
-                        signal(SIGINT, ntb_main_context_quit_signal_cb);
-                mc->old_term_handler =
-                        signal(SIGTERM, ntb_main_context_quit_signal_cb);
-        }
-}
-
 struct ntb_main_context_source *
 ntb_main_context_add_quit(struct ntb_main_context *mc,
                           ntb_main_context_quit_callback callback,
                           void *user_data)
 {
-        struct ntb_main_context_source *source =
-                ntb_slice_alloc(&ntb_main_context_source_allocator);
+        struct ntb_main_context_source *source;
 
         if (mc == NULL)
                 mc = ntb_main_context_get_default_or_abort();
+
+        pthread_mutex_lock(&mc->idle_mutex);
+        source = ntb_slice_alloc(&mc->source_allocator);
+        mc->n_sources++;
+        pthread_mutex_unlock(&mc->idle_mutex);
 
         source->mc = mc;
         source->callback = callback;
@@ -332,11 +364,6 @@ ntb_main_context_add_quit(struct ntb_main_context *mc,
         source->user_data = user_data;
 
         ntb_list_insert(&mc->quit_sources, &source->quit_link);
-
-        mc->n_sources++;
-
-        if (mc->quit_pipe_source == NULL)
-                create_quit_pipe_source(mc);
 
         return source;
 }
@@ -366,11 +393,15 @@ ntb_main_context_add_timer(struct ntb_main_context *mc,
                            ntb_main_context_timer_callback callback,
                            void *user_data)
 {
-        struct ntb_main_context_source *source =
-                ntb_slice_alloc(&ntb_main_context_source_allocator);
+        struct ntb_main_context_source *source;
 
         if (mc == NULL)
                 mc = ntb_main_context_get_default_or_abort();
+
+        pthread_mutex_lock(&mc->idle_mutex);
+        source = ntb_slice_alloc(&mc->source_allocator);
+        mc->n_sources++;
+        pthread_mutex_unlock(&mc->idle_mutex);
 
         source->mc = mc;
         source->bucket = get_bucket(mc, minutes);
@@ -380,9 +411,14 @@ ntb_main_context_add_timer(struct ntb_main_context *mc,
 
         ntb_list_insert(&source->bucket->sources, &source->timer_link);
 
-        mc->n_sources++;
-
         return source;
+}
+
+static void
+wakeup_main_loop(struct ntb_main_context *mc)
+{
+        if (!pthread_equal(pthread_self(), mc->main_thread))
+                send_async_byte(mc, 'W');
 }
 
 struct ntb_main_context_source *
@@ -390,20 +426,25 @@ ntb_main_context_add_idle(struct ntb_main_context *mc,
                           ntb_main_context_idle_callback callback,
                           void *user_data)
 {
-        struct ntb_main_context_source *source =
-                ntb_slice_alloc(&ntb_main_context_source_allocator);
+        struct ntb_main_context_source *source;
 
         if (mc == NULL)
                 mc = ntb_main_context_get_default_or_abort();
+
+        /* This may be called from a thread other than the main one so
+         * we need to guard access to the idle sources lists */
+        pthread_mutex_lock(&mc->idle_mutex);
+        source = ntb_slice_alloc(&mc->source_allocator);
+        ntb_list_insert(&mc->idle_sources, &source->idle_link);
+        mc->n_sources++;
+        pthread_mutex_unlock(&mc->idle_mutex);
 
         source->mc = mc;
         source->callback = callback;
         source->type = NTB_MAIN_CONTEXT_IDLE_SOURCE;
         source->user_data = user_data;
 
-        ntb_list_insert(&mc->idle_sources, &source->idle_link);
-
-        mc->n_sources++;
+        wakeup_main_loop(mc);
 
         return source;
 }
@@ -430,7 +471,9 @@ ntb_main_context_remove_source(struct ntb_main_context_source *source)
                 break;
 
         case NTB_MAIN_CONTEXT_IDLE_SOURCE:
+                pthread_mutex_lock(&mc->idle_mutex);
                 ntb_list_remove(&source->idle_link);
+                pthread_mutex_unlock(&mc->idle_mutex);
                 break;
 
         case NTB_MAIN_CONTEXT_TIMER_SOURCE:
@@ -445,9 +488,10 @@ ntb_main_context_remove_source(struct ntb_main_context_source *source)
                 break;
         }
 
-        ntb_slice_free(&ntb_main_context_source_allocator, source);
-
+        pthread_mutex_lock(&mc->idle_mutex);
+        ntb_slice_free(&mc->source_allocator, source);
         mc->n_sources--;
+        pthread_mutex_unlock(&mc->idle_mutex);
 }
 
 static int
@@ -535,12 +579,26 @@ emit_idle_sources(struct ntb_main_context *mc)
         struct ntb_main_context_source *source, *tmp_source;
         ntb_main_context_timer_callback callback;
 
+        pthread_mutex_lock(&mc->idle_mutex);
+
+        /* This loop needs to cope with sources being added from other
+         * threads while iterating. It doesn't need to cope with
+         * sources being removed, apart from the one currently being
+         * executed. Any new sources would be added at the beginning
+         * of the list so they shouldn't cause any problems and they
+         * would just be missed by this loop */
+
         ntb_list_for_each_safe(source, tmp_source,
                                &mc->idle_sources,
                                idle_link) {
                 callback = source->callback;
+
+                pthread_mutex_unlock(&mc->idle_mutex);
                 callback(source, source->user_data);
+                pthread_mutex_lock(&mc->idle_mutex);
         }
+
+        pthread_mutex_unlock(&mc->idle_mutex);
 }
 
 static void
@@ -588,15 +646,20 @@ void
 ntb_main_context_poll(struct ntb_main_context *mc)
 {
         int n_events;
+        int n_sources;
 
         if (mc == NULL)
                 mc = ntb_main_context_get_default_or_abort();
 
-        if (mc->n_sources > mc->events_size) {
+        pthread_mutex_lock(&mc->idle_mutex);
+        n_sources = mc->n_sources;
+        pthread_mutex_unlock(&mc->idle_mutex);
+
+        if (n_sources > mc->events_size) {
                 ntb_free(mc->events);
                 mc->events = ntb_alloc(sizeof (struct epoll_event) *
-                                       mc->n_sources);
-                mc->events_size = mc->n_sources;
+                                       n_sources);
+                mc->events_size = n_sources;
         }
 
         n_events = epoll_wait(mc->epoll_fd,
@@ -672,19 +735,18 @@ ntb_main_context_free(struct ntb_main_context *mc)
 {
         ntb_return_if_fail(mc != NULL);
 
-        if (mc->quit_pipe_source) {
-                signal(SIGINT, mc->old_int_handler);
-                signal(SIGTERM, mc->old_term_handler);
-                ntb_main_context_remove_source(mc->quit_pipe_source);
-                close(mc->quit_pipe[0]);
-                close(mc->quit_pipe[1]);
-        }
+        signal(SIGINT, mc->old_int_handler);
+        signal(SIGTERM, mc->old_term_handler);
+        ntb_main_context_remove_source(mc->async_pipe_source);
+        close(mc->async_pipe[0]);
+        close(mc->async_pipe[1]);
 
         if (mc->n_sources > 0)
                 ntb_warning("Sources still remain on a main context "
                             "that is being freed");
 
         ntb_free(mc->events);
+        pthread_mutex_destroy(&mc->idle_mutex);
         close(mc->epoll_fd);
         ntb_free(mc);
 
