84be03d9d012f8749de77fd102a8e38c7b8d7fe0
Neil Roberts <bpeeluk@yahoo.co.uk>
2013-12-23 11:05:37 +0000

    Add ntb_pointer_array
    This is just a thin wrapper around ntb_buffer to help with managing an
    array of pointers.

---
+++ src/ntb-pointer-array.h
@@ -0,0 +1,42 @@
+/*
+ * Notbit - A Bitmessage client
+ * Copyright (C) 2013  Neil Roberts
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef NTB_POINTER_ARRAY_H
+#define NTB_POINTER_ARRAY_H
+
+#include <stdint.h>
+
+#include "ntb-buffer.h"
+
+static inline void
+ntb_pointer_array_append(struct ntb_buffer *buf,
+                         void *pointer)
+{
+        ntb_buffer_append(buf, &pointer, sizeof (pointer));
+}
+
+#define ntb_pointer_array_length(buf)           \
+        ((buf)->length / sizeof (void *))
+
+#define ntb_pointer_array_get(buf, index)       \
+        ((((void **) (buf)->data))[index])
+
+#define ntb_pointer_array_set(buf, index, pointer)      \
+        ((((void **) (buf)->data))[index] = (pointer))
+
+#endif /* NTB_POINTER_ARRAY_H */
