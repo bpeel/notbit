45f307503368770933f2ba193ea86f56d4391555
Neil Roberts <bpeeluk@yahoo.co.uk>
2014-02-04 01:22:33 +0000

    store: Use MIME's encoded words to store subject headers if need be
    If the subject header contains non-ascii characters or is too long it
    will now be encoded using base64 in encoded words so that it has a
    better chance of being understood by the email clients.

--- src/ntb-store.c
+++ src/ntb-store.c
@@ -41,6 +41,7 @@
 #include "ntb-main-context.h"
 #include "ntb-file-error.h"
 #include "ntb-base58.h"
+#include "ntb-base64.h"
 #include "ntb-address.h"
 #include "ntb-load-keys.h"
 
@@ -833,6 +834,76 @@ generate_maildir_name(struct ntb_store *store,
         }
 }
 
+static bool
+need_encoded_words(const uint8_t *subject,
+                   size_t subject_length)
+{
+        size_t i;
+
+        if (subject_length > 76 - 9)
+                return true;
+
+        for (i = 0; i < subject_length; i++)
+                if (subject[i] >= 128 || subject[i] < ' ')
+                        return true;
+
+        if (subject_length > 0) {
+                for (i = 0; i < subject_length - 1; i++) {
+                        if (subject[i] == '=' && subject[i + 1] == '?')
+                                return true;
+                }
+        }
+
+        return false;
+}
+
+static void
+write_encoded_words(const uint8_t *subject,
+                    size_t subject_length,
+                    FILE *out)
+{
+        /* Maximum line length allowed is 76 and the first line will
+         * contain ‘Subject: ’ so we want to account for that. Base64
+         * grows the size by a quarter. Each line contains the extra
+         * characters ‘=?UTF-8?B?’ and ‘?=’ */
+        const size_t max_encoded_length = 76 - 9 - 10 - 2;
+        const size_t max_chunk_length = max_encoded_length / 4 * 3;
+        char buf[(max_chunk_length * 4 + 2) / 3];
+        size_t encoded_size;
+        size_t chunk_size;
+
+        while (true) {
+                chunk_size = MIN(max_chunk_length, subject_length);
+
+                encoded_size = ntb_base64_encode(subject, chunk_size, buf);
+
+                assert(encoded_size <= sizeof buf);
+
+                fputs("=?UTF-8?B?", out);
+                fwrite(buf, 1, encoded_size, out);
+                fputs("?=", out);
+
+                subject += chunk_size;
+                subject_length -= chunk_size;
+
+                if (subject_length <= 0)
+                        break;
+
+                fputs("\n ", out);
+        }
+}
+
+static void
+write_subject(const uint8_t *subject,
+              size_t subject_length,
+              FILE *out)
+{
+        if (need_encoded_words(subject, subject_length))
+                write_encoded_words(subject, subject_length, out);
+        else
+                fwrite(subject, 1, subject_length, out);
+}
+
 static void
 save_message(struct ntb_store *store,
              time_t timestamp,
@@ -880,8 +951,7 @@ save_message(struct ntb_store *store,
             !memcmp(msg.message, "Subject:", 8) &&
             (eol = memchr(msg.message, '\n', msg.message_length))) {
                 fputs("Subject: ", out);
-
-                fwrite(msg.message + 8, 1, eol - msg.message - 8, out);
+                write_subject(msg.message + 8, eol - msg.message - 8, out);
                 fputc('\n', out);
 
                 msg.message_length -= eol - msg.message + 1;
